f35d69746aec70394161b1ec4f7ff496601519cf
matteo <matteo.barberis@epfl.ch>
2025-11-28 17:48:00 +0100

    Single carrier preamble

--- matlab_code_ofdm/rxofdm.m
+++ matlab_code_ofdm/rxofdm.m
@@ -10,5 +10,16 @@ function [rxbits, conf] = rxofdm(rxsignal,conf)
 %   rxbits      : received bits
 %
 
+%% Transmission Parameters
+
+
+%% Downconversion
+% Keep in mind that downconversion is done once for the entire signal
+rxsignal_downconverted = down_conversion(rxsignal,conf.f_c,conf.f_s);
+
+%% Matched filtering ONLY FOR PREAMBLE
+% Remember that the OFDM signal has just a low pass filter
+
+
 rxbits = zeros(conf.nbits,1);
 end
--- matlab_code_ofdm/txofdm.m
+++ matlab_code_ofdm/txofdm.m
@@ -7,4 +7,15 @@ function [txsignal, conf] = txofdm(txbits,conf)
 %   txbits  : Information bits
 %   conf    : Universal configuration structure
 
+%% Transmission parameters
+
+
+%% Preamble generation and mapping
+preamble = preamble_generate(conf.sc.nsyms); % generate preamble of 100 bits (no need to map for bpsk)
+preamble_mapped = -2 .* preamble + 1; % bpsk_modulate the preamble
+preamble_upsample = upsample(preamble_mapped,conf.sc.os_factor); %OFDM has a different OS factor
+preamble_shaped = conv(preamble_upsample, conf.sc.txpulse, 'valid');
+preamble_upconverted = up_conversion(preamble_shaped, conf.f_c, conf.f_s);
+
 txsignal = zeros(10000,1);
+txsignal(1:length(preamble_upconverted)) = preamble_upconverted;
