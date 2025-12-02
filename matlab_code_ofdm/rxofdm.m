b870efc4a994386cefb4cb273a7b594c4d135537
matteo <matteo.barberis@epfl.ch>
2025-12-02 09:28:28 +0100

    Single carrier frame sync

--- matlab_code_ofdm/rxofdm.m
+++ matlab_code_ofdm/rxofdm.m
@@ -17,8 +17,17 @@ function [rxbits, conf] = rxofdm(rxsignal,conf)
 % Keep in mind that downconversion is done once for the entire signal
 rxsignal_downconverted = down_conversion(rxsignal,conf.f_c,conf.f_s);
 
+%% Filter the Downconverted RX Signal
+
+% Apply the low pass filter given
+f_cutoff = conf.ofdm.bandwidth + 0.05 * conf.ofdm.bandwidth;      % Define the filter cutoff as 5% above the baseband BW
+rx_signal_filtered = ofdmlowpass(rxsignal_downconverted, conf, f_cutoff);
+
 %% Matched filtering ONLY FOR PREAMBLE
 % Remember that the OFDM signal has just a low pass filter
+
+%% Frame Synchronization
+OFDM_init = frame_sync(rx_signal_filtered, conf);    % First sample of the OFDM signal
 
 
 rxbits = zeros(conf.nbits,1);
