f257c76b2265ed0eba108972e1d13747817992ef
Federico Vassallo <federico.vassallo@epfl.ch>
2025-12-24 10:34:15 +0100

    Update demapper.m

--- matlab_code_ofdm/demapper.m
+++ matlab_code_ofdm/demapper.m
@@ -1,4 +1,13 @@
 function [b] = demapper(symbol)
+%DEMAPPER Demaps complex QPSK symbols into a binary bit stream
+%   Converts the equalized complex symbols back into the original logical
+%   bit sequence based on the quadrant they occupy.
+%
+%   INPUTS
+%   - symbol: Vector or matrix of complex QPSK symbols
+%
+%   OUTPUTS
+%   - b: Column vector of the recovered binary bits
 
 bit1 = real(symbol) > 0;
 bit2 = imag(symbol) > 0;
