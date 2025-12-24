3786b0193272c95543a5ab17084bf1f7c57e8648
Federico Vassallo <federico.vassallo@epfl.ch>
2025-12-24 10:41:11 +0100

    Update matched_filter.m

--- matlab_code_ofdm/matched_filter.m
+++ matlab_code_ofdm/matched_filter.m
@@ -1,12 +1,16 @@
 function [filtered_signal, h] = matched_filter(signal, os_factor, mf_length)
-% Create a root-raised cosine filter and filter the signal with it. It
-% outputs the full convolution sequence, hence it is necessary to remove
-% the pulse shape tx filter's size from the beginning and the matched
-% filter's size from the end of the output of this function. These size can
-% be obtained with length(pulse)
-% Inputs:
-% os_factor is the oversampling factor (4 in our course)
-% mf_length is the one-sided filter length. The total number of filter coefficients is 2*mf_length + 1, that is the center tap and mf_length taps to both sides.
+%MATCHED_FILTER Filters the signal with a root-raised cosine pulse for synchronization.
+%   Convolves the input signal with a Root-Raised Cosine (RRC) filter to maximize SNR
+%   at the optimal sampling instant, aiding in precise symbol timing recovery.
+%
+%   INPUTS
+%   - signal: The input signal vector
+%   - os_factor: Oversampling factor (samples per symbol)
+%   - mf_length: One-sided length of the filter in symbols
+%
+%   OUTPUTS
+%   - filtered_signal: The output of the convolution (full length)
+%   - h: The filter coefficients used
     
     rolloff_factor = 0.22;
     
