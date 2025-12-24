79f021f0a4a614f2c5f0e9282dcfb8c327ed27ba
Federico Vassallo <federico.vassallo@epfl.ch>
2025-12-24 10:39:14 +0100

    Update training_generate.m

--- matlab_code_ofdm/training_generate.m
+++ matlab_code_ofdm/training_generate.m
@@ -1,4 +1,13 @@
 function output = training_generate(output_length)
+%TRAINING_GENERATE Generates the pseudo-random sequence used for pilot symbols.
+%   Uses a Linear Feedback Shift Register (LFSR) to create a known binary sequence,
+%   which is then mapped to QPSK to serve as training pilots or channel estimation headers.
+%
+%   INPUTS
+%   - output_length: Desired length of the training sequence in bits
+%
+%   OUTPUTS
+%   - output: Column vector containing the generated pseudo-random bits
 
     polynomial = [1 1 0 0 0 1 1 0]';
     
